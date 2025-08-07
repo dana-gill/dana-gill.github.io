d500753da17bac72f06ee92754a2e75ad7a98028
dana-gill <danagslee@gmail.com>
2025-08-07 17:20:07 +0200

    edit root margin and cursor

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const observer = new IntersectionObserver((entries) => {
   });
 }, {
   threshold: 0.1,
-  rootMargin: '0px 0px 200px 0px'
+  rootMargin: '0px 0px 100px 0px'
 });
 
 document.addEventListener('DOMContentLoaded', () => {
