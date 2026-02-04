8efb8f32a66fbc1681b46dae739ce616bb0a9c80
dana-gill <danagslee@gmail.com>
2026-02-04 12:50:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+"use strict";
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('animate');
-    }
-  });
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
+    });
 }, {
-  threshold: 0.1,
-  rootMargin: '0px 0px 100px 0px'
+    threshold: 0.1,
+    rootMargin: '0px 0px 100px 0px'
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  const isSmallScreen = window.outerWidth <= 768;
-
-  if (isSmallScreen) {
+const applyMobileAnimation = () => {
     const allElements = document.querySelectorAll('h1, h2, h3, h4, p, .highlight');
     allElements.forEach((el) => el.classList.add('mobile-animation'));
     allElements.forEach((el) => el.classList.add('animate'));
-  } else {
+};
+const applyDesktopAnimation = () => {
     const allElements = document.querySelectorAll('h1, h2, h3, h4, p, .highlight');
     allElements.forEach((el) => observer.observe(el));
-  }
+};
+document.addEventListener('DOMContentLoaded', () => {
+    const isSmallScreen = window.outerWidth <= 768;
+    if (isSmallScreen) {
+        applyMobileAnimation();
+        return;
+    }
+    applyDesktopAnimation();
 });
